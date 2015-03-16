f47102d66c8f08a6de84c6279415c97c0a342313
Brannon Dorsey <brannon@brannondorsey.com>
2015-03-15 22:10:43 -0500

    Add server.js and data/

---
+++ node/server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/../public'));
+
+var server = app.listen(3000, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log('Server listening at http://%s:%s', host, port)
+
+});
