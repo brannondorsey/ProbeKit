06cef4b7bd259aba84169a86f5d26c72487fed22
Brannon Dorsey <brannon@brannondorsey.com>
2015-03-17 00:30:34 +0000

    Probes are now being parsed to csv correctly with node/tshark_process_pipe.js

--- node/tshark_process_pipe.js
+++ node/tshark_process_pipe.js
@@ -1,19 +1,53 @@
+var fs = require('fs');
 var spawn = require('child_process').spawn;
+var spawnSync = require('child_process').spawnSync;
+var moment = require('moment');
 var argv = require('minimist')(process.argv.slice(2));
+var TsharkProbeParser = require('./src/TsharkProbeParser');
 
 if (argv.interface == undefined) {
-	console.log('Usage: node scriptname --interface=<interface_name>');
+	console.log('Usage: node scriptname --interface=<interface_name> [--output=probes.csv]');
 	process.exit(1);
 }
 
-var TsharkProbeParser = require('./src/TsharkProbeParser');
+// check for ifconfig
+var proc = spawnSync('which', ['ifconfig']);
+if (proc.status != 0) {
+	console.log('ifconfig not found, please install ifconfig');
+	process.exit(1);
+}
+
+// check for iwconfig
+proc = spawnSync('which', ['iwconfig']);
+if (proc.status != 0) {
+	console.log('iwconfig not found, please install iwconfig');
+	process.exit(1);
+}
+
+// check for tshark
+proc = spawnSync('which', ['tsark']);
+if (proc.status != 0) {
+	console.log('tshark not found, please install tshark with:\n\tsudo apt-get install tshark');
+	process.exit(1);
+}
+
+var outputFile = argv.output || __dirname + '/../data/probes.csv';
+
+var writeStream = fs.createWriteStream(outputFile, { flags: 'a', encoding: 'utf8' });
+
 var probeParser = new TsharkProbeParser();
 
-tsharkProcess = spawn('tshark', ['-i', argv.interface, '-n', '-I', '-l']); //, 'subtype', 'probereq'
+tsharkProcess = spawn('tshark', ['-i', argv.interface, '-n', '-I', '-l', 'subtype', 'probereq']);
 
-// tshark should be line buffered with -l flag
 tsharkProcess.stdout.on('data', function (data) {
-	probeParser.parseLine(data + '');
+	
+	var lines = data.toString('utf8').split('\n');
+	
+	for (var i = 0; i < lines.length; i++) {
+		// this will fire the probeParser.on('probeReceived') event
+		// if the packet is parsed successfully
+		probeParser.parseLine(lines[i]);
+	}
 });
 
 tsharkProcess.stderr.on('data', function (data) {
@@ -21,10 +55,18 @@ tsharkProcess.stderr.on('data', function (data) {
 });
 
 tsharkProcess.on('close', function (code) {
-  console.log('child process exited with code ' + code);
+  console.log('tshark process exited with code ' + code);
+  writeStream.close();
 });
 
 // register event first
 probeParser.on('probeReceived', function(packet){
-	console.log(packet.mac + ',' + packet.ssid + ',' + packet.timestamp);
+	
+	var csvLine = packet.mac + ',' + packet.ssid + ',' + packet.timestamp + '\n'
+	writeStream.write(csvLine);
+
+	console.log('MAC:  ' + packet.mac);
+	console.log('SSID: ' + packet.ssid);
+	console.log('Time: ' + moment(packet.timestamp).format('YYYY-MM-DD HH:mm:ss'));
+	console.log();
 });
