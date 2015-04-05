da94bb275f3a24763a541acf267278bbb0aef514
Brannon Dorsey <brannon@brannondorsey.com>
2015-04-05 15:37:31 -0500

    Working on wigle data download. Not yet working but maybe close.

--- node/src/WigleBatchDownloader.js
+++ node/src/WigleBatchDownloader.js
@@ -4,8 +4,8 @@ var _ = require('underscore');
 function WigleBatchDownloader(username, password, callback) {
 	
 	var self = this;
-	self.requestTimeout = 2000; // request timeout so as not to bog down server
-	self.maxConcurrentRequests = 100;
+	self.requestTimeout = 1000 * 60; // default
+	// self.maxConcurrentRequests = 1;
 	self.downloading = false;
 
 	self.wigleClient = wigle.createClient(username, password, callback);
@@ -31,50 +31,47 @@ function WigleBatchDownloader(username, password, callback) {
 // 		onlymine: ""
 // 	}
 // }
-WigleBatchDownloader.prototype.download = function(options, callback){
+WigleBatchDownloader.prototype.download = function(options, requestCallback, callback){
 	
 	var self = this;
 
 	self.downloading = true;
+	if (options.requestTimeout) self.requestTimeout = options.requestTimeout;
 
 	var chunkObjs = self.getChunkObjects(parseFloat(options.fence.latrange1),
 										 parseFloat(options.fence.latrange2),
 										 parseFloat(options.fence.longrange1),
 										 parseFloat(options.fence.longrange2),
 										 parseFloat(options.chunkSize));
-	
-	var numRequests = 10; // chunkObjs.length;
+	var numRequests = 3; // chunkObjs.length;
+	var requestCounter = 0;
 	var afterCallback = _.after(numRequests, callback);
 	var allNetworks = [];
 
-	// for (var i = 0; i < chunkObjs.length; i++) {
-	for (var i = 0; i < 10; i++) {
-		
-		var chunk = chunkObjs[i];
-		
-		if (options.filter) {
-			var filer = options.filter;
-			if (filter.ssid) chunk.ssid         = filter.ssid;
-			if (filter.lastupdt) chunk.lastupdt = filter.lastupdt;
-			if (filter.netid) chunk.netid       = filter.netid;
-			if (filter.freenet) chunk.freenet   = filter.freenet;
-			if (filter.paynet) chunk.paynet     = filter.paynet;
-			if (filter.dhcp) chunk.dhcp         = filter.dhcp;
-			if (filter.onlymine) chunk.onlymine = filter.onlymine;
-		}
+	// Make only one query at a time so as not to bog down server.
+	// recursively calls onQueryChunkReceived() until requestCounter == numRequests
+	self._queryChunk(options, chunkObjs[requestCounter], onQueryChunkReceived);
 
-		chunk.simple = true;
-		
-		self.wigleClient.query(chunk, function(err, result) {
-			
-			if (err) throw err;
-			
-			console.log('Timestamp:', result.timestamp);
-			console.log('Number of networks found:', result.networks.length);
-			// console.log('Networks:', result.networks);
-			allNetworks.concat(result.networks);
-			afterCallback(allNetworks);
-		});
+	function onQueryChunkReceived(err, result) {
+
+		if (err) throw err;
+
+		console.log('onQueryChunkReceived: request finished.');
+
+		requestCounter++;
+		allNetworks = allNetworks.concat(result.networks);
+		requestCallback(result);
+		afterCallback(allNetworks);
+
+		// if there are more requests left
+		if (requestCounter < numRequests) {
+			console.log('onQueryChunkReceived: More requests left. Waiting ' + self.requestTimeout + 'ms before making another request.');
+			setTimeout(function(){
+				console.log('onQueryChunkReceived: timeout finished. Making request.');
+				console.log(chunkObjs[requestCounter]);
+				self._queryChunk(options, chunkObjs[requestCounter], onQueryChunkReceived);
+			}, self.requestTimeout);
+		}
 	}
 }
 
@@ -86,15 +83,35 @@ WigleBatchDownloader.prototype.getChunkObjects = function(latrange1, latrange2, 
 	for (var x = latrange1; x < latrange2; x += chunkSize) {
 		for (var y = longrange1; y < longrange2; y += chunkSize) {
 			chunkObjs.push({
-				latrange1: x,
-				latrange2: x + chunkSize,
-				longrange1: y,
-				longrange2: y + chunkSize
+				latrange1: parseFloat(x.toFixed(4)),
+				latrange2: parseFloat((x + chunkSize).toFixed(4)),
+				longrange1: parseFloat(y.toFixed(4)),
+				longrange2: parseFloat((y + chunkSize).toFixed(4))
 			});
 		}
 	}
 
 	return chunkObjs;
+}
+
+WigleBatchDownloader.prototype._queryChunk = function(options, chunk, callback){
+
+	var self = this;
+
+	if (options.filter) {
+		var filter = options.filter;
+		if (filter.ssid) chunk.ssid         = filter.ssid;
+		if (filter.lastupdt) chunk.lastupdt = filter.lastupdt;
+		if (filter.netid) chunk.netid       = filter.netid;
+		if (filter.freenet) chunk.freenet   = filter.freenet;
+		if (filter.paynet) chunk.paynet     = filter.paynet;
+		if (filter.dhcp) chunk.dhcp         = filter.dhcp;
+		if (filter.onlymine) chunk.onlymine = filter.onlymine;
+	}
+
+	chunk.simple = true;
+	
+	self.wigleClient.query(chunk, callback);
 }
 
 module.exports = WigleBatchDownloader;
--- node/wigle_dump.js
+++ node/wigle_dump.js
@@ -1,38 +1,33 @@
-// var argv = require('minimist')(process.argv.slice(2));
-
-// var outputFile = argv.output || __dirname + '/../data/probes.csv';
-// var writeStream = fs.createWriteStream(outputFile, { flags: 'a', encoding: 'utf8' });
-
-// if (argv.interface == undefined) {
-// 	console.log('Usage: node scriptname --interface=<interface_name> [--output=probes.csv]');
-// 	process.exit(1);
-// }
-
+var fs = require('fs');
 var WigleBatchDownloader = require('./src/WigleBatchDownloader');
 
-var batchDownloader = new WigleBatchDownloader('46ea0d5b2', '46ea0d5b2', function(){
+// 46ea0d5b2
+var batchDownloader = new WigleBatchDownloader('brannon', 'wiglepassword', function(){
 	// fires on wigle.net login
 
 	var options = {
 		chunkSize: 0.005,
+		requestTimeout: 1000 * 60, // 1 minute between requests to not bog down server
 		fence: {
-			latrange1: 41.8489,
-			latrange2: 41.9141,
-			longrange1: -87.7357,
-			longrange2: -87.5383
+			latrange1: 41.8756, // must be less than latrange2
+			latrange2: 41.8864,
+			longrange1: -87.6365, // must be less than longrange2
+			longrange2: -87.6242
+		},
+		filter: {
+			lastupdt: 20100101000000 // format: YYYYMMDDHHMMSS
 		}
-		// filter: {
-		// 	ssid: "",
-		// 	lastupdt: "",
-		// 	netid: "",
-		// 	freenet: "",
-		// 	paynet: "",
-		// 	dhcp: "",
-		// 	onlymine: ""
-		// }
 	}
 
-	batchDownloader.download(options, function(networks){
-		console.log('Download finished, ' + networks.length + ' networks downloaded.');
-	});
+	batchDownloader.download(options, onRequestFinished, onAllNetworksDownloaded);
+
+	function onRequestFinished(result) {
+		// result.timestamp, result.networks
+		console.log('Query returned ' + result.networks.length + ' at ' + result.timestamp);
+	}
+
+	function onAllNetworksDownloaded(allNetworks) {
+		console.log('All queries have returned. ' + allNetworks.length + ' downloaded.');
+		fs.writeFile(__dirname + '/../data/');
+	}
 });
