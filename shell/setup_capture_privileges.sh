967317f86a9fba30118fa98c624dfcc1e52cc3ab
Brannon Dorsey <brannon@brannondorsey.com>
2015-04-27 18:35:07 -0400

    Update setup_capture_privileges.sh

--- shell/setup_capture_privileges.sh
+++ shell/setup_capture_privileges.sh
@@ -17,30 +17,37 @@
 OS=$(uname)
 SCRIPT_NAME=$(basename $0)
 DIR_NAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
-#Must be root
-if [[ $EUID -ne 0 ]]; then
-    echo "[$SCRIPT_NAME] This script must be run as root:" #1>&2
-    echo "    sudo ./$SCRIPT_NAME"
-    exit 1
-fi
-
-if [[ $OS == "Linux" ]] || [[ $OS == "Darwin" ]]; then
-
-    if [[ $OS == "Linux" ]]; then
-        linux_capture_privileges();
-    else
-        osx_capture_privileges();
-    fi
-else
-    echo "This script is only supported on Linux and OSX platforms";
-    exit 1;
-fi
+ISSUES_LINK="https://github.com/brannondorsey/ProbeRequestCollectorsKit/issues"
 
 function osx_capture_privileges() {
 
-    tar zxf "$DIR/../data/ChmodBPF.tar.gz" --directory "$DIR/../data"
-    open "$DIR/../data/ChmodBPF/Install ChmodBPF.app"
+    local TAR="$DIR_NAME/../data/ChmodBPF.tar.gz"
+
+    which tshark 2>/dev/null >/dev/null
+
+    if [[ $? -ne 0 ]]; then
+        echo "[$SCRIPT_NAME] Wireshark is not installed. Try running 'sudo ./install.sh' or install wireshark with:"
+        echo "  brew install wireshark"
+        exit 1
+    fi
+
+    if [ -f "$TAR" ]; then
+        
+        tar zxf "$TAR" --directory "$DIR_NAME/../data"
+
+        if [[ $? -eq 0 ]]; then
+            open "$DIR_NAME/../data/ChmodBPF/Install ChmodBPF.app"
+        else
+            echo "[$SCRIPT_NAME] Error: $TAR could not be extracted with tar."
+            exit 1
+        fi
+
+    else
+        echo "[$SCRIPT_NAME] Error: $TAR does not exist"
+        exit 1
+    fi
+
+    return 0
 }
 
 function linux_capture_privileges() {
@@ -57,10 +64,45 @@ function linux_capture_privileges() {
     dpkg-reconfigure wireshark-common 
     usermod -a -G wireshark $USER
     
-    echo "[$SCRIPT_NAME] Your capture privileges have been setup correctly. Would you like to restart now? (Y/n):"
-    read SURE;
-
-    if [[ $SURE == "Y" ]] || [[ $SURE == "y" ]] || [[ $SURE == "" ]]; then
-        gnome-session-quit --logout --no-prompt
-    fi
+    return 0
 }
+
+#Must be root
+if [[ $EUID -ne 0 ]]; then
+    echo "[$SCRIPT_NAME] This script must be run as root:"
+    echo "    sudo ./$SCRIPT_NAME"
+    exit 1
+fi
+
+if [[ $OS == "Linux" ]] || [[ $OS == "Darwin" ]]; then
+
+    if [[ $OS == "Linux" ]]; then
+        linux_capture_privileges
+    else
+        osx_capture_privileges
+    fi
+
+    if [[ $? -eq 0 ]]; then
+
+        echo "[$SCRIPT_NAME] Your capture privileges have been setup correctly."
+
+        if [[ $OS == "Linux" ]]; then
+            
+            echo "A restart is required. Would you like to restart now? (Y/n):"
+            read SURE;
+
+            if [[ $SURE == "Y" ]] || [[ $SURE == "y" ]] || [[ $SURE == "" ]]; then
+                gnome-session-quit --logout --no-prompt
+            fi
+        fi
+
+    else # theoretically this should never be reached as errors should have already terminated script
+        echo "[$SCRIPT_NAME] There was an error setting up your capture privileges. Check out $ISSUES_LINK\
+        for more info or to submit a bug report."
+        exit 1;
+    fi
+
+else
+    echo "[$SCRIPT_NAME] This script is only supported on Linux and OSX platforms"
+    exit 1
+fi
