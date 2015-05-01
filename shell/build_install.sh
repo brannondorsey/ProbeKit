6eec3e1ce5a6c628e7da452f1386adc7e0da74ea
Brannon Dorsey <brannon@brannondorsey.com>
2015-05-01 01:04:22 -0500

    add build_install.sh

---
+++ shell/build_install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#=======================================================================
+#
+#          FILE:  build_install.sh
+#
+#         USAGE:  ./build_install.sh
+#
+#       OPTIONS:  ./build_install.sh
+#  REQUIREMENTS:  ---
+#         NOTES:  ---
+#        AUTHOR: Brannon Dorsey, <brannon@brannondorsey.com>
+#       COMPANY:  ---
+#       CREATED: 05.01.2015
+#      REVISION: 0.0.1
+#=======================================================================
+
+# osx only for now
+cd "Probe Kit.app/Contents/Resources/app.nw/shell"
+exec ./install.sh
